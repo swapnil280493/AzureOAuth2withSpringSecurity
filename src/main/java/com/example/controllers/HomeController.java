7183a8faae728076aea773f0c67671aaa3e55bbd
swapnil280493 <swapnildhage01@gmail.com>
2022-12-10 00:52:36 -0800

    updated the files to meet the requirement

--- src/main/java/com/example/controllers/HomeController.java
+++ src/main/java/com/example/controllers/HomeController.java
@@ -1,19 +1,16 @@
 package com.example.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @RestController
 public class HomeController {
 
-	@GetMapping("/")
-	public ModelAndView home() {
+	@RequestMapping("/")
+	public String home() {
 		
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("Hi! from azure");
-		mv.setViewName("home.jsp");
-		return mv ;
+		return "Successfully logged in";
 	}
 }
